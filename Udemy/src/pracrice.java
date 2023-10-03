4dd98b825a6c6b008c571ec7c057611a670f66e0
Sanketr123 <rautsanket33@gmail.com>
2023-10-03 23:24:45 +0530

    alert and dropdown added

--- Udemy/src/pracrice.java
+++ Udemy/src/pracrice.java
@@ -1,58 +1,136 @@
 //import java.io.*;
 import java.util.Scanner;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class pracrice {
 
 	public static void main(String[] args) {
 		
-		Scanner obj = new Scanner(System.in);
 		
-		System.out.println("Enter first num");
-		int a = obj.nextInt();
 		
-		System.out.println("Enter  operator");
 		
-		char operator = obj.next().charAt(0);
-		
-		System.out.println("Enter second num");
-		int b= obj.nextInt();
-		
-		int z;
-
-		
-		switch(operator) {
-		
-		case '+':
-			z=a+b;
-			System.out.println("Result: " + z);
-			break;	
-		case '-':
-			z=a-b; 
-			System.out.println("Result: " + z);
-			break;	
-		case '*':
-			z=a*b;
-			System.out.println("Result: " + z);
-			break;	
-		case '/':
-			if (b!= 0) {
-				
-				z=a/b;
-				System.out.println("Result: " + z);
-			}
-			else {
-				System.out.println("Invalid number");
-			}
-			
-			break;	
-			
-		default:
-			System.out.println("invalid operator");
-		
-		}
-		
-		obj.close();
-
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int result = sum(5,10);
+//System.out.println(result);
+//
+//}
+//
+//public static int sum(int start,int end) {
+//if(end>start) {
+//	return end+sum(start,end-1);}
+//else {
+//	return end;
+//	
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+//int result = sum(10);
+//System.out.println(result);
+//
+//}
+//
+//public static int sum(int k) {
+//if(k>0) {
+//	return k + sum(k-1);
+//}
+//else {
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+//Scanner obj = new Scanner(System.in);
+//
+//System.out.println("Enter first num");
+//int a = obj.nextInt();
+//
+//System.out.println("Enter  operator");
+//
+//char operator = obj.next().charAt(0);
+//
+//System.out.println("Enter second num");
+//int b= obj.nextInt();
+//
+//int z;
+//
+//
+//switch(operator) {
+//
+//case '+':
+//	z=a+b;
+//	System.out.println("Result: " + z);
+//	break;	
+//case '-':
+//	z=a-b; 
+//	System.out.println("Result: " + z);
+//	break;	
+//case '*':
+//	z=a*b;
+//	System.out.println("Result: " + z);
+//	break;	
+//case '/':
+//	if (b!= 0) {
+//		
+//		z=a/b;
+//		System.out.println("Result: " + z);
+//	}
+//	else {
+//		System.out.println("Invalid number");
+//	}
+//	
+//	break;	
+//	
+//default:
+//	System.out.println("invalid operator");
+//
+//}
+//
+//obj.close();
